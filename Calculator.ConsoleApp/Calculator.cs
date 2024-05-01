d717d5af56baf640a4b8aef9ecb03e38a22d0258
iansalazar444 <151499351+iansalazar444@users.noreply.github.com>
2024-05-01 23:12:32 +0800

    remove other code in preparation of demo

--- Calculator.ConsoleApp/Calculator.cs
+++ Calculator.ConsoleApp/Calculator.cs
@@ -9,26 +9,9 @@ namespace Calculator.ConsoleApp
 {
     public class CalculatorApp
     {
-        private readonly ILogger<CalculatorApp> _logger;
-        private readonly ICalculatorResultCache _calculatorResultCache;
-
-        public CalculatorApp(ILogger<CalculatorApp> logger, ICalculatorResultCache cache)
-        {
-            _logger = logger;
-            _calculatorResultCache = cache;
-        }
-
         public async Task<decimal> Divide(decimal dividend, decimal divisor)
         {
-            // do some normal division
-            if (divisor == 0)
-            {
-                throw new ActivityException();
-            }
-            var result = dividend / divisor;
-            await _calculatorResultCache.Execute(result);
-
-            return result;
+            throw new NotImplementedException();    
         }
     }
 }
